3d0ef21eb54bde5b57b1131a647efb885491d8c8
CMOISDEAD <camilodavina@gmail.com>
2023-05-13 11:53:53 -0500

    feat(master): product component

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 
-import { Product } from 'src/models/product.model';
-
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent {
-  content = 'content and more';
-  @Input() product!: Product;
+  @Input()
+  banner = {
+    title: '',
+    image_url: '',
+    path: '',
+  };
 }
